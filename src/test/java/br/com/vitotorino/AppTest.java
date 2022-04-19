129d7411cfbbf9e20ad7b6cd1e968ba2c1299926
Sergio Vitorino <sergiovlvitorino@gmail.com>
2022-04-19 13:19:00 -0300

    first

---
+++ src/test/java/br/com/vitotorino/AppTest.java
@@ -0,0 +1,20 @@
+package br.com.vitotorino;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
